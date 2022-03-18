503218e8d10324ae824f6d89dd193de616c6863b
Oskar Miller <oskar.miller2001@gmail.com>
2022-03-18 09:29:07 +0000

    flake fix

--- src/lib/features/single_frame.py
+++ src/lib/features/single_frame.py
@@ -15,19 +15,21 @@ def mice_distance_angle(frame: np.ndarray) -> np.ndarray(dtype=float, shape=(3, 
      [i][j][0] is distance between mouse i and j,
      [i][j][1] is angle between mouse i and j
     """
-    mice = [Sequence.mouse_from_frame(frame, 0),
-            Sequence.mouse_from_frame(frame, 1),
-            Sequence.mouse_from_frame(frame, 2)]
+    mice = [
+        Sequence.mouse_from_frame(frame, 0),
+        Sequence.mouse_from_frame(frame, 1),
+        Sequence.mouse_from_frame(frame, 2),
+    ]
     result = np.zeros((3, 3, 2), dtype=float)
     for m1 in range(3):
-        neck = Sequence.name_mouse(mice[m1])['neck']
-        nose = Sequence.name_mouse(mice[m1])['nose']
+        neck = Sequence.name_mouse(mice[m1])["neck"]
+        nose = Sequence.name_mouse(mice[m1])["nose"]
         direction = nose - neck
         direction_norm = np.linalg.norm(direction)
         for m2 in range(3):
             if m1 == m2:
                 continue
-            second_neck = Sequence.name_mouse(mice[m2])['neck']
+            second_neck = Sequence.name_mouse(mice[m2])["neck"]
             intermouse = second_neck - neck
             result[m1][m2][0] = np.linalg.norm(intermouse)
             intermouse_norm = np.linalg.norm(intermouse)
@@ -48,31 +50,42 @@ def calc_mouse_length(frame: np.ndarray) -> np.ndarray(dtype=float, shape=(3)):
     :param frame: original keypoints - 72 numbers
     :return: float array
     """
-    mice = [Sequence.mouse_from_frame(frame, 0),
-            Sequence.mouse_from_frame(frame, 1),
-            Sequence.mouse_from_frame(frame, 2)]
+    mice = [
+        Sequence.mouse_from_frame(frame, 0),
+        Sequence.mouse_from_frame(frame, 1),
+        Sequence.mouse_from_frame(frame, 2),
+    ]
     lengths = np.zeros((3), dtype=float)
     for m in range(3):
-        lengths[m] = np.linalg.norm(Sequence.name_mouse(mice[m])['nose'] - Sequence.name_mouse(mice[m])['tail base'])
+        lengths[m] = np.linalg.norm(
+            Sequence.name_mouse(mice[m])["nose"] - Sequence.name_mouse(mice[m])["tail base"]
+        )
     return lengths
 
 
 def calc_mean_reach(frame: np.ndarray) -> np.ndarray(dtype=float, shape=(3)):
     """
-    calculates the mean distance from keypoints to 'center back' for each mouse (excluding the tail)
+    calculates the mean distance from keypoints to 'center back' for each mouse (excluding the
+    tail)
     :param frame: original keypoints - 72 numbers
     :return: float array
     """
-    mice = [Sequence.mouse_from_frame(frame, 0),
-            Sequence.mouse_from_frame(frame, 1),
-            Sequence.mouse_from_frame(frame, 2)]
+    mice = [
+        Sequence.mouse_from_frame(frame, 0),
+        Sequence.mouse_from_frame(frame, 1),
+        Sequence.mouse_from_frame(frame, 2),
+    ]
     reach = np.zeros((3), dtype=float)
     for m in range(3):
         distances = []
         for keypoint in Sequence.name_mouse(mice[m]):
-            if keypoint not in ['center back', 'tail middle', 'tail tip']:
-                distances.append(np.linalg.norm(
-                    Sequence.name_mouse(mice[m])[keypoint] - Sequence.name_mouse(mice[m])['center back']))
+            if keypoint not in ["center back", "tail middle", "tail tip"]:
+                distances.append(
+                    np.linalg.norm(
+                        Sequence.name_mouse(mice[m])[keypoint]
+                        - Sequence.name_mouse(mice[m])["center back"]
+                    )
+                )
         reach[m] = np.mean(distances)
 
     return reach
@@ -86,14 +99,16 @@ def calc_head_angle(frame: np.ndarray) -> np.ndarray(dtype=float, shape=(3)):
     :param frame: original keypoints - 72 numbers
     :return: float array
     """
-    mice = [Sequence.mouse_from_frame(frame, 0),
-            Sequence.mouse_from_frame(frame, 1),
-            Sequence.mouse_from_frame(frame, 2)]
+    mice = [
+        Sequence.mouse_from_frame(frame, 0),
+        Sequence.mouse_from_frame(frame, 1),
+        Sequence.mouse_from_frame(frame, 2),
+    ]
     result = np.zeros((3), dtype=float)
     for m in range(3):
-        nose = Sequence.name_mouse(mice[m])['nose']
-        neck = Sequence.name_mouse(mice[m])['neck']
-        tail_base = Sequence.name_mouse(mice[m])['tail base']
+        nose = Sequence.name_mouse(mice[m])["nose"]
+        neck = Sequence.name_mouse(mice[m])["neck"]
+        tail_base = Sequence.name_mouse(mice[m])["tail base"]
 
         body_direction = neck - tail_base
         head_direction = nose - neck
@@ -102,8 +117,9 @@ def calc_head_angle(frame: np.ndarray) -> np.ndarray(dtype=float, shape=(3)):
         head_direction_norm = np.linalg.norm(head_direction)
 
         if body_direction_norm != 0 and head_direction_norm != 0:
-            dot_product = np.dot(body_direction, head_direction) / \
-                          body_direction_norm / head_direction_norm
+            dot_product = (
+                np.dot(body_direction, head_direction) / body_direction_norm / head_direction_norm
+            )
             dot_product = np.clip(dot_product, -1, 1)
             angle = np.arccos(dot_product)
             result[m] = angle
