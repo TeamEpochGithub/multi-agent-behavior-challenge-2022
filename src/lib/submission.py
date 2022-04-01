b22f995d752587bb0727daf35999ed44b43b6a04
mirceatlx <mirceatlx@gmail.com>
2022-04-01 10:33:07 +0200

    more fixes

--- src/lib/submission.py
+++ src/lib/submission.py
@@ -1,10 +1,13 @@
 import numpy as np
 from torch import nn
+import torch
 from tqdm import tqdm
+
 
 def find_seq(name, sub_sequences):
     """
-    Finds the index of the sequence. Useful for features computed outside the function submission_embeddings
+    Finds the index of the sequence. 
+    Useful for features computed outside the function submission_embeddings
     :param name: id of the sequence
     :param sub_sequences: list of sequences
     """
@@ -15,44 +18,52 @@ def find_seq(name, sub_sequences):
     raise ValueError
 
 
-def submission_embeddings(config: dict, sub_clips: dict, model: nn.Module, sub_seq: list, func = None, embd: list = None) -> dict:
+def submission_embeddings(config: dict, sub_clips: dict, model: nn.Module, sub_seq: list, func=None, embd: list=None) -> dict:
     """
-
-    Creates a ready for submission dict with an arbitrary model (not true yet).
-    :param config: configuration dict for different variables 
-    :param sub_clips: submission_clips 
+    Creates a ready for submission dict with an arbitrary model.
+    (works only for the perceiver for now)
+    :param config: configuration dict for different variables
+    :param sub_clips: submission_clips
     :param model: trained model for submission
-    :param sub_seq: list of sequences, used for find_seq function 
+    :param sub_seq: list of sequences, used for find_seq function
     :param func: list of single-frame feature functions
     :param embd: list of np.ndarrays of precomputed features (e.g velocity)
-    :return: dict ready for submission (however should be validated with the validate_submission function before using it)
+    :return: dict ready for submission
+    (however should be validated with the validate function before using it)
     """
 
     embeddings_model_size = config["model_embd_size"]
     features_size = config["feature_size"]
     num_total_frames = np.sum([seq['keypoints'].shape[0] for _, seq in sub_clips['sequences'].items()])
     embeddings_size = embeddings_model_size + features_size
+    embeddings_array = np.empty((num_total_frames, embeddings_size), dtype = np.float32)
 
     if embeddings_size > 128:
-        raise ValueError(f"The maximum number of embeddings is 128, you have {embeddings_size}")
+        raise ValueError(f'The maximum number of embeddings is 128, you have {embeddings_size}')
 
     frame_number_map = {}
     start = 0
     seq_len = config["seq_len"]
-
 
     for sequence_key in tqdm(sub_clips["sequences"]):
         keypoints = sub_clips["sequences"][sequence_key]["keypoints"]
         embeddings = np.empty((len(keypoints), embeddings_size), dtype = np.float32)
 
         X = np.array([keypoints[i].flatten() for i in range(0, seq_len, config["frame_increment"])])
-        embs = model(torch.Tensor(X).cuda().unsqueeze(0), return_embeddings = config["return_embeddings"])[0] # probably works only for the perceiver for now
+        
+        # probably works only for the perceiver for now
+        embs = model(torch.Tensor(X).cuda().unsqueeze(0), 
+                return_embeddings=config["return_embeddings"])[0] 
 
         for i in range(len(keypoints)):
 
-            if i % config["freq_embd_calc"] == 0 and i + seq_len < len(keypoints): # in the initial notebook config["freq_embd_calc"] is 100, ideally should be 1 but really long submission time
+            if i % config["freq_embd_calc"] == 0 and i + seq_len < len(keypoints): 
+                # in the initial notebook config["freq_embd_calc"] is 100, 
+                #ideally should be 1 but really long submission time
                 X = np.array([keypoints[i].flatten() for i in range(0, seq_len, config["frame_increment"])])
-                embs = model(torch.Tensor(X).cuda().unsqueeze(0), return_embeddings = config["return_embeddings"])[0] # probably works only for the perceiver for now
+                # probably works only for the perceiver for now
+                embs = model(torch.Tensor(X).cuda().unsqueeze(0), 
+                        return_embeddings=config["return_embeddings"])[0] 
 
         embeddings[i, :embeddings_model_size] = embs.detach().cpu().numpy()
         last = embeddings_model_size
@@ -60,7 +71,6 @@ def submission_embeddings(config: dict, sub_clips: dict, model: nn.Module, sub_s
             temp_values = f(keypoints[i].flatten())
             embeddings[i, last: last + temp_values.shape[0]] = temp_values
             last += temp_values.shape[0]
-
 
     seq_index = find_seq(sequence_key, sub_seq)
     # in the notebook you have full energies here, needs to be passed in embd
@@ -77,7 +87,6 @@ def submission_embeddings(config: dict, sub_clips: dict, model: nn.Module, sub_s
     submission_dict = {"frame_number_map": frame_number_map, "embeddings": embeddings_array}
 
     return submission_dict
-
 
 def validate_submission(submission, submission_clips):
     """
@@ -130,9 +139,9 @@ def validate_submission(submission, submission_clips):
 
 def save_submission_file(filename, sub_dict: dict):
     """
-    Saves the submission dict as an npy file. 
+    Saves the submission dict as an npy file.
     :param filename: name of the new submission file
-    :param sub_dict: dict for submission 
+    :param sub_dict: dict for submission
     :return:
     """
     np.save(filename, sub_dict)
